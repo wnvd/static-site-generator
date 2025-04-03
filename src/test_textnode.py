7ed1bbaef226a592e1a6b53f58ff05385c1d263f
wnvd <waninaveed998@gmail.com>
2025-04-03 11:12:09 +0530

    added and function to convert text to html also added some text

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -22,6 +22,33 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is alt for image", TextType.IMAGE, "path/to/image")
         node2 = TextNode("This is alt for image", TextType.IMAGE, "path/to/image2")
         self.assertNotEqual(node, node2)
+
+class TestTextToHTML(unittest.TestCase):
+    def test_text_to_html(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold_text_to_html(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold text node")
+
+    def test_link_text_to_html(self):
+        node = TextNode("this is anchor text", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "this is anchor text")
+        self.assertEqual(html_node.props, { "href": "https://www.boot.dev" })
+
+    def test_img_text_to_html(self):
+        node = TextNode("sunshine", TextType.IMAGE, "/img/sunshine.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, None)
+        self.assertEqual(html_node.props, { "src": "/img/sunshine.png", "alt": "sunshine" })
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -24,3 +25,20 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": f"{text_node.url}"} )
+        case TextType.IMAGE:
+            return LeafNode("img", None, { "src": f"{text_node.url}", "alt": f"{text_node.text}"})
+        case _:
+            raise Exception("invalid text node type")
