9cfdcc1963d618f9fabbcf99573e2bf467170146
wnvd <waninaveed998@gmail.com>
2025-04-11 14:00:03 +0530

    moved copy_static function into a separate file

---
+++ src/copy_static.py
@@ -0,0 +1,16 @@
+import os, shutil
+
+def copy_static(src, dst):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    os.mkdir(dst)
+
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item) 
+        dst_path = os.path.join(dst, item)
+
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dst_path)
+            print(f"Copied file: {src_path} to {dst_path}")
+        else:
+            copy_static(src_path, dst_path)
--- src/main.py
+++ src/main.py
@@ -1,21 +1,10 @@
 from textnode import TextNode, TextType
 from gencontent import extract_title, generate_page
-import os, shutil
+from copy_static import copy_static
 
-def copy_static(src, dst):
-    if os.path.exists(dst):
-        shutil.rmtree(dst)
-    os.mkdir(dst)
+def genereate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    pass
 
-    for item in os.listdir(src):
-        src_path = os.path.join(src, item) 
-        dst_path = os.path.join(dst, item)
-
-        if os.path.isfile(src_path):
-            shutil.copy(src_path, dst_path)
-            print(f"Copied file: {src_path} to {dst_path}")
-        else:
-            copy_static(src_path, dst_path)
 
 def main():
 
