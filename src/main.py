66357d7198af1d133d0bd96aeab8bcbbc462bbfb
wnvd <waninaveed998@gmail.com>
2025-04-11 09:19:11 +0530

    add function to copy static files to public to serve

--- src/main.py
+++ src/main.py
@@ -1,10 +1,26 @@
 from textnode import TextNode, TextType
+import os, shutil
 
-print("hello world")
+def copy_static(src, dst):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    os.mkdir(dst)
+
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item) 
+        dst_path = os.path.join(dst, item)
+
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dst_path)
+            print(f"Copied file: {src_path} to {dst_path}")
+        else:
+            copy_static(src_path, dst_path)
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev/")
-    print(node)
+
+    src = "static/"
+    dst = "public/"
+    copy_static(src, dst)
 
 
 if __name__ == "__main__":
